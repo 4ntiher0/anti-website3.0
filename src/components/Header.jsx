08722c1ac01948fc0c88c61d2e9bfdfaf66e7b15
4ntiher0 <clemeons@gmail.com>
2025-09-01 03:59:27 -0400

    change logo location and center navi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import './header.css';
-import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="headertxt">
-        <img src={logo} alt="ANTIs ARENA Logo" className="header-logo" />
+        {/* Use public folder directly, no import */}
+        <img src="/logo.png" alt="ANTIs ARENA Logo" className="header-logo" />
       </div>
       <div className="headerlogo">
-        <div className="cube">
-          <div className="face front">Front</div>
-          <div className="face back">Back</div>
-          <div className="face right">Right</div>
-          <div className="face left">Left</div>
-          <div className="face top">Top</div>
-          <div className="face bottom">Bottom</div>
-        </div>
+
       </div>
     </header>
   );
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,11 +1,18 @@
-
 import React, { useState } from "react";
+import './projects.css';
 
 function Projects() {
 
   return (
   <div className="section">
-Nothing to see.
+        <div className="cube">
+          <div className="face front">Front</div>
+          <div className="face back">Back</div>
+          <div className="face right">Right</div>
+          <div className="face left">Left</div>
+          <div className="face top">Top</div>
+          <div className="face bottom">Bottom</div>
+        </div>
 </div>
 
 
