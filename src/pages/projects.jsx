ccfbd104c3e0fe7ef814341c12db5c8863dfa0e2
4ntiher0 <clemeons@gmail.com>
2025-09-01 15:26:11 -0400

    changed cude on projects page to include MTG symbols

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
-import './projects.css';
+import "./projects.css";
+import white from '../assets/symbols/plains.svg';
+import black from '../assets/symbols/swamp.svg';
+import red from '../assets/symbols/mountain.svg';
+import blue from '../assets/symbols/island.svg';
+import green from '../assets/symbols/forest.svg';
+import death from '../assets/symbols/death.svg';
 
 function Projects() {
-
   return (
-  <div className="section">
-    <div className="cubewrap">
+    <div className="section">
+      <div className="cubewrap">
         <div className="cube">
-          <div className="face front">Front</div>
-          <div className="face back">Back</div>
-          <div className="face right">Right</div>
-          <div className="face left">Left</div>
-          <div className="face top">Top</div>
-          <div className="face bottom">Bottom</div>
+          <div className="face front">
+            <img src={white} alt="White Mana" />
+          </div>
+          <div className="face back">
+            <img src={blue} alt="Blue Mana" />
+          </div>
+          <div className="face right">
+            <img src={black} alt="Black Mana" />
+          </div>
+          <div className="face left">
+            <img src={red} alt="Red Mana" />
+          </div>
+          <div className="face top">
+            <img src={green} alt="Green Mana" />
+          </div>
+          <div className="face bottom">
+            <img src={death} alt="Death Mana" />
+          </div>
         </div>
       </div>
-  </div>
-    
-);
+    </div>
+  );
 }
 
 export default Projects;
